c8d142253ba03b6323f3f7880d58874381f7792f
Aishwarya Sharma <ishans632@gmail.com>
2018-03-11 18:38:00 +0530

    Add DELETE todos/:id route

--- server/server.js
+++ server/server.js
@@ -53,6 +53,24 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
+//DELETE
+app.delete('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 // Listening to port 3000
 app.listen(3000, () => {
   console.log('Started on port 3000');
